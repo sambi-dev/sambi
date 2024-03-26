937ffacab8dace1141e605564726e711600c9ab3
Sam Rizvi <srizvi@users.noreply.github.com>
2024-03-26 03:56:18 -0700

    style: clean up logos #177
    - cleaned up logos
    - refined site description
    - cleared cache and rebuilt packages etc.

--- apps/web/src/ui/warnings/hero-warning.tsx
+++ apps/web/src/ui/warnings/hero-warning.tsx
@@ -3,17 +3,17 @@ import { StarRating } from '#/ui/shared/star-rating';
 export function HeroWarning() {
   return (
     <figure className="relative mx-auto max-w-md text-center lg:mx-0 lg:text-left">
-      <div className="flex justify-center text-secondary-foreground lg:justify-start">
+      <div className="flex justify-center text-alternate lg:justify-start">
         <StarRating />
       </div>
       <blockquote className="mt-2">
-        <p className="text-balance text-lg font-medium text-secondary-foreground">
+        <p className="text-balance text-lg font-medium text-muted-foreground">
           “Recommend Yo CXO? No thank you. Listen, just stay away. I depend on
           them too much to share.”
         </p>
       </blockquote>
       <figcaption className="mt-2 text-sm text-muted-foreground">
-        <strong className="font-mono tracking-tighter text-primary before:content-['—_']">
+        <strong className="font-mono tracking-tighter text-muted-foreground before:content-['—_']">
           John G.
         </strong>
       </figcaption>
