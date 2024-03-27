3cc63858ecb614360e09d1685aafa423efe89468
Sam Rizvi <srizvi@users.noreply.github.com>
2024-03-27 01:07:47 -0700

    feat: cache auth ine rsc #174

---
+++ packages/auth/src/config.ts
@@ -0,0 +1,32 @@
+import type { DefaultSession, NextAuthConfig } from 'next-auth';
+
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import Discord from 'next-auth/providers/discord';
+
+import { db, tableCreator } from '@yocxo/db';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession['user'];
+  }
+}
+
+export const authConfig = {
+  adapter: DrizzleAdapter(db, tableCreator),
+  providers: [Discord],
+  callbacks: {
+    session: (opts) => {
+      if (!('user' in opts)) throw 'unreachable with session strategy';
+
+      return {
+        ...opts.session,
+        user: {
+          ...opts.session.user,
+          id: opts.user.id,
+        },
+      };
+    },
+  },
+} satisfies NextAuthConfig;
