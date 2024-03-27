9881fa6b070e89ac4ab790c2c54cec9646f36228
Sam Rizvi <srizvi@users.noreply.github.com>
2024-03-27 00:40:33 -0700

    chore: improve ts performance #174

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -1,11 +1,13 @@
+import type { TRPCRouterRecord } from '@trpc/server';
+
 import { z } from 'zod';
 
 import { desc, eq, schema } from '@yocxo/db';
 import { CreatePostSchema } from '@yocxo/validators';
 
-import { createTRPCRouter, protectedProcedure, publicProcedure } from '../trpc';
+import { protectedProcedure, publicProcedure } from '../trpc';
 
-export const postRouter = createTRPCRouter({
+export const postRouter = {
   all: publicProcedure.query(({ ctx }) => {
     // return ctx.db.select().from(schema.post).orderBy(desc(schema.post.id));
     return ctx.db.query.post.findMany({
@@ -36,4 +38,4 @@ export const postRouter = createTRPCRouter({
   delete: protectedProcedure.input(z.number()).mutation(({ ctx, input }) => {
     return ctx.db.delete(schema.post).where(eq(schema.post.id, input));
   }),
-});
+} satisfies TRPCRouterRecord;
