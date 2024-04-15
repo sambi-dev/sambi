480d367ae83bfdfa691fcf8ec130e76b979187fb
Sam Rizvi <srizvi@users.noreply.github.com>
2024-04-15 05:05:54 -0700

    chore: update deps #230
    - revert to relative import to fix typecheck?
    - update some dependencies
    
    Link #230

--- apps/mai/src/data/screens.ts
+++ apps/mai/src/data/screens.ts
@@ -1,20 +1,20 @@
 import type { StaticImageData } from 'next/image';
 
-import image1 from '#/images/1.jpg';
-import image2 from '#/images/2.jpg';
-import image3 from '#/images/3.jpg';
-import image4 from '#/images/4.jpg';
-import image5 from '#/images/5.jpg';
-import image6 from '#/images/6.jpg';
-import image7 from '#/images/7.jpg';
-import image8 from '#/images/8.jpg';
-import image9 from '#/images/9.jpg';
-import image10 from '#/images/10.jpg';
-import image11 from '#/images/11.jpg';
-import image12 from '#/images/12.jpg';
-import image13 from '#/images/13.jpg';
-import image16 from '#/images/16.jpg';
-import image17 from '#/images/17.jpg';
+import image1 from '../images/1.jpg';
+import image2 from '../images/2.jpg';
+import image3 from '../images/3.jpg';
+import image4 from '../images/4.jpg';
+import image5 from '../images/5.jpg';
+import image6 from '../images/6.jpg';
+import image7 from '../images/7.jpg';
+import image8 from '../images/8.jpg';
+import image9 from '../images/9.jpg';
+import image10 from '../images/10.jpg';
+import image11 from '../images/11.jpg';
+import image12 from '../images/12.jpg';
+import image13 from '../images/13.jpg';
+import image16 from '../images/16.jpg';
+import image17 from '../images/17.jpg';
 
 export interface Screen {
   title: string;
