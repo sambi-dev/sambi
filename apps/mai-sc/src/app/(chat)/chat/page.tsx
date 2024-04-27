0e75da87a8c3e4c29a45b00f55dd260cb5d60e3a
Sam Rizvi <srizvi@users.noreply.github.com>
2024-04-27 03:47:26 -0700

    chore: fixed more redirects #283

--- apps/mai-sc/src/app/(chat)/chat/page.tsx
+++ apps/mai-sc/src/app/(chat)/chat/page.tsx
@@ -1,6 +1,8 @@
 import type { Session } from '#/lib/types';
 import type { Metadata } from 'next';
 
+import { redirect } from 'next/navigation';
+
 import { auth } from 'auth';
 
 import { getMissingKeys } from '#/app/actions';
@@ -13,6 +15,10 @@ export default async function IndexPage() {
   const id = nanoid();
   const session = (await auth()) as Session;
   const missingKeys = await getMissingKeys();
+
+  if (!session?.user) {
+    redirect(`/login`);
+  }
 
   return (
     <AI initialAIState={{ chatId: id, messages: [] }}>
